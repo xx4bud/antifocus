2c1565fee7e91ac968ee8fecf98ca5809a6a8bd2
abud16810 <abud16810@gmail.com>
2025-02-01 03:38:43 +0700

    Set Fix

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -2,8 +2,6 @@
 export default function HomePage() {
   return (
     <div className="container flex flex-1 flex-col">
-      {/* <CampaignsList />
-      <ProductsList /> */}
       <div className="flex flex-col gap-4 py-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
