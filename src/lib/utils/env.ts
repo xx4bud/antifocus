ef4440dd08f3f28a2791275d7492de4cb1dd9fdd
Ridwan Abud <51215760+xx4bud@users.noreply.github.com>
2026-04-04 09:31:31 +0000

    fix: reset

--- src/lib/utils/env.ts
+++
@@ -1,5 +0,0 @@
-export const isClient = typeof window !== "undefined";
-export const isServer = !isClient;
-
-export const isDevelopment = process.env.NODE_ENV === "development";
-export const isProduction = process.env.NODE_ENV === "production";
